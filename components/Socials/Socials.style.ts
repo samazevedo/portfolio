cb0ee42378b2133634da6d4e702e476f7e5092ba
samazevedo <samuel.azevedo@live.com>
2022-05-12 17:53:19 -0500

    fixed icons padding

--- components/Socials/Socials.style.ts
+++ components/Socials/Socials.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.ul`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    padding: 0 5rem 0 5rem;
+    padding: 0 2rem 0 2rem;
 `
 export const SocialItem = styled.li`
     font-size: 1rem;
