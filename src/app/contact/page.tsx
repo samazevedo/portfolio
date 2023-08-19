cafb89341e49d84a612be9676401e3e66e4e4596
samazevedo <samuel.azevedo@live.com>
2023-08-18 19:34:58 -0700

    add contact form without functionality yet

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,55 @@
 'use client'
-import { Container, Text } from '@chakra-ui/react'
+import { Section } from '@/components/dom/section'
+import { Paragraph } from '@/components/styles/styles'
+import {
+    Container,
+    FormControl,
+    FormLabel,
+    Text,
+    Input,
+    Textarea,
+    FormHelperText,
+    Button,
+} from '@chakra-ui/react'
 
 export default function Page() {
     return (
         <Container pt={20}>
-            <Text as='h2' fontSize={30} mb={4}>
-                Contact
-            </Text>
+            <Section>
+                <Text as='h2' fontSize={30} mb={4}>
+                    Contact
+                </Text>
+                <Paragraph>
+                    For any questions or future work, please send me an email or
+                    use the form below to send me a message.
+                </Paragraph>
+                <FormControl>
+                    <FormLabel>Name</FormLabel>
+                    <Input
+                        type='text'
+                        placeholder='Full Name'
+                        variant='flushed'
+                        focusBorderColor='primary.green'
+                    />
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                        type='email'
+                        variant='flushed'
+                        mb={5}
+                        placeholder='Email Address'
+                        focusBorderColor='primary.green'
+                    />
+                    <FormLabel>Message</FormLabel>
+                    <Textarea
+                        placeholder='Your message'
+                        focusBorderColor='primary.green'
+                    />
+                    <FormHelperText></FormHelperText>
+                    <Button size='md' bg='primary.green'>
+                        Submit
+                    </Button>
+                </FormControl>
+            </Section>
         </Container>
     )
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,19 @@
 'use client'
+import { Section } from '@/components/dom/section'
+import { Paragraph } from '@/components/styles/styles'
 import { Container, Text } from '@chakra-ui/react'
 
 export default function Page() {
     return (
         <Container pt={20}>
-            <Text as='h2' fontSize={30} mb={4}>
-                Portfolio
-            </Text>
+            <Section>
+                <Text as='h2' fontSize={30} mb={4}>
+                    Portfolio
+                </Text>
+                <Paragraph>
+                    Here is some of my latest works and projects.
+                </Paragraph>
+            </Section>
         </Container>
     )
 }
