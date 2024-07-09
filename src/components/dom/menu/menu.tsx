ca1d3feb41b5102f2ef231c123aa014de5201c7d
samazevedo <samuel.azevedo@live.com>
2024-07-09 15:47:04 -0700

    fixing menu item size

--- src/components/dom/menu/menu.tsx
+++ src/components/dom/menu/menu.tsx
@@ -16,7 +16,7 @@ export default function Menu() {
 				side="top"
 				className="w-[100vw] h-[100vh] opacity-70 grid grid-rows-2 gap-2 p-2 "
 			>
-				<div className="grid gap-2 py-6 w-full h-full text-center mt-40 first:text-8xl sm:text-5xl font-bold ">
+				<div className="grid gap-2 py-6 w-full h-full text-center pt-40 lg:text-8xl md:text-5xl sm:text-5xl font-bold ">
 					<SmoothLink
 						href="home"
 						prefetch={false}
